17c76e83fa021efe5ed0e60e6e147bb34003d14f
psy888 <tbilisi.zp.snab@gmail.com>
2020-03-05 11:50:17 +0200

    init

---
+++ src/main/java/com/psy888/book/library/Application.java
@@ -0,0 +1,16 @@
+package com.psy888.book.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.Resource;
+
+@SpringBootApplication
+
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/psy888/book/library/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psy888.book.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
