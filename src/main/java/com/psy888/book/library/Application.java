7249a72d6f1869105f392a11acfc6156e53c0aad
psy888 <tbilisi.zp.snab@gmail.com>
2020-03-05 17:42:24 +0200

    add write/read test methods

--- src/main/java/com/psy888/book/library/Application.java
+++ src/main/java/com/psy888/book/library/Application.java
@@ -1,16 +1,95 @@
 package com.psy888.book.library;
 
+import com.psy888.book.library.model.AuthorEntity;
+import com.psy888.book.library.model.BookEntity;
+import com.psy888.book.library.model.GenreEnum;
+import com.psy888.book.library.service.BookLibraryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import javax.annotation.Resource;
+import java.time.LocalDate;
+import java.time.Year;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @SpringBootApplication
-
+@Slf4j
 public class Application {
+
+	@Autowired
+	private BookLibraryService bookLibraryService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+
+	@Bean
+	public CommandLineRunner run (){
+		return args -> {
+			addTestRoman();
+			addTestFantasy();
+			printAuthorBooks();
+			printFilteredBooks();
+		};
+	}
+
+
+	public void addTestRoman(){
+		BookEntity newRoman = new BookEntity();
+		newRoman.setBooking("Подвал на улице Ленина");
+		newRoman.setName("Армено и Желетта");
+		newRoman.setPagesCount(300);
+		newRoman.setReleaseDate(Year.of(2017));
+		newRoman.setGenre(GenreEnum.ROMAN);
+
+		AuthorEntity author = new AuthorEntity();
+		author.setFirstName("Инокентий");
+		author.setSecondName("Педросович");
+		author.setLastName("Пупырышкин");
+		author.setBirthDate(LocalDate.of(1990,4,12));
+		newRoman.setAuthor(author);
+		bookLibraryService.addNewBook(newRoman);
+	}
+
+	public void addTestFantasy(){
+		BookEntity newFantasy = new BookEntity();
+		newFantasy.setBooking("Крыша на улице Чекистов");
+		newFantasy.setName("Пластелин Колец");
+		newFantasy.setPagesCount(800);
+		newFantasy.setReleaseDate(Year.of(2010));
+		newFantasy.setGenre(GenreEnum.FANTASY);
+
+		AuthorEntity author = bookLibraryService.findAuthorByFullName(
+				"Инокентий",
+				"Педросович",
+				"Пупырышкин"
+		);
+		newFantasy.setAuthor(author);
+		bookLibraryService.addNewBook(newFantasy);
+	}
+
+	public void printAuthorBooks(){
+		List<BookEntity> books = bookLibraryService.findBooksByAuthor(
+				"Инокентий",
+				"Педросович",
+				"Пупырышкин"
+		);
+		books.forEach(book -> log.info(book.toString()));
+	}
+
+	public void printFilteredBooks(){
+		Set<GenreEnum> genres = new HashSet<>();
+		genres.add(GenreEnum.FANTASY);
+		genres.add(GenreEnum.ROMAN);
+
+		List<BookEntity> books = bookLibraryService.findBooksByGenre(genres);
+		books.forEach(book->log.info(book.toString()));
+	}
 }
---
+++ src/test/java/com/psy888/book/library/service/BookLibraryServiceTest.java
@@ -0,0 +1,28 @@
+package com.psy888.book.library.service;
+
+import com.psy888.book.library.model.AuthorEntity;
+import com.psy888.book.library.model.BookEntity;
+import com.psy888.book.library.model.GenreEnum;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.Year;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookLibraryServiceTest {
+
+    @Test
+    void addNewBook() {
+
+
+    }
+
+    @Test
+    void findBooksByGenre() {
+    }
+
+    @Test
+    void findBooksByAuthor() {
+    }
+}
